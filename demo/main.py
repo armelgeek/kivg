976bf83fb41990d475fff04caca906c87e28fae0
Hajavololona Armellin <57889510+armelgeek@users.noreply.github.com>
2025-11-26 07:02:37 +0300

    Update SVG drawing parameters in demo
    Changed the SVG drawing to enable fill, set line width to 1, and decreased animation duration for a faster effect. These adjustments improve the visual appearance and speed of the demo animation.

--- demo/main.py
+++ demo/main.py
@@ -101,11 +101,11 @@ class KivgDemo(App):
         self.s.draw(
             svg_file, 
             animate=True, 
-            fill=False,              # Désactiver le remplissage pour voir le tracé
-            line_width=3,            # Trait plus épais pour mieux voir
+            fill=True,              # Désactiver le remplissage pour voir le tracé
+            line_width=1,            # Trait plus épais pour mieux voir
             show_hand=True,          # Afficher la main qui dessine
             hand_size=(150, 150),    # Main plus grande pour mieux voir
-            dur=0.02,                # Animation un peu plus lente
+            dur=0.002,                # Animation un peu plus lente
             pen_offset=(25, 120)     # Ajuster la position du stylo (x, y depuis le coin supérieur gauche de l'image)
         )
     
